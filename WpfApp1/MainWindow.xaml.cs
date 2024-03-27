aa8f644d15dd562385f226666bc781f423452d89
Mahmoud Samy <mahmodsamy102@gmail.com>
2024-03-27 15:44:30 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool StateClosed = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PART_TITLEBAR_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void PART_MAXIMIZE_RESTORE_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Width = 1080;
+                Height = 720;
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void PART_MINIMIZE_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void PART_CLOSE_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ClickCount==2)
+            {
+                if (WindowState == WindowState.Normal)
+                {
+                    WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    Width = 1080;
+                    Height = 720;
+                    WindowState = WindowState.Normal;
+                }
+            }
+            else
+            {
+                DragMove();
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void TopButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if(StateClosed)
+            {
+                Storyboard sb1 = FindResource("OpenMargin") as Storyboard;
+                Storyboard sb =FindResource("OpenMenu") as Storyboard;
+                sb.Begin();
+                sb1.Begin();
+            }
+            else
+            {
+                Storyboard sb1 = FindResource("CloseMargin") as Storyboard;
+                Storyboard sb = FindResource("CloseMenu") as Storyboard;
+                sb.Begin();
+                sb1.Begin();
+            }
+            StateClosed = !StateClosed;
+        }
+
+        private void BtnNoty_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/UserControls/SideMenu.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SideMenu.xaml
+    /// </summary>
+    public partial class SideMenu : UserControl
+    {
+        public SideMenu()
+        {
+            InitializeComponent();
+        }
+        private bool StateClosed = true;
+        private void TbWhareHouse_Checked(object sender, RoutedEventArgs e)
+        {
+            
+                GridSubItem.Visibility = Visibility.Visible;
+                Storyboard sb = FindResource("OpenSubMenu") as Storyboard;
+                sb.Begin();
+           
+        }
+
+        private void TbWhareHouse_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = FindResource("CloseSubMenu") as Storyboard;
+            sb.Begin();
+            GridSubItem.Visibility = Visibility.Collapsed;
+        }
+    }
+}
